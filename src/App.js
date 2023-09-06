d84cc38b61a71b2ea23967bbe75bc5db903195a4
Dawid Pampuch <dawid.pampuch97@gmail.com>
2023-09-06 21:01:18 +0200

    setAllDone interaction

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
-  }
+  };
 
   const removeTask = (id) => {
     setTasks(tasks => tasks.filter(task => task.id !== id));
@@ -29,7 +29,14 @@ function App() {
 
       return task;
     }));
-  }
+  };
+
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true,
+    })))
+  };
 
   return (
     <Container>
@@ -53,6 +60,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
